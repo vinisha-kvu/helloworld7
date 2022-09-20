b54022c1818a99fbadccae7afe6340c01c8e404b
vinisha.kalevaru <vinisha.kalevaru@diva-e.com>
2022-09-20 12:55:07 +0200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="text-5xl flex items-center justify-center">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
